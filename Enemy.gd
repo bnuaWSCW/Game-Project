6907c79ae2c1458d5cc0070b0bbc072d57852b1a
bnuaWSCW <nuab@wsc.school.nz>
2024-08-26 20:43:59 +1200

    Brandon

---
+++ Enemy.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+# The Speed of the Character
+@export var SPEED = 30
+@onready var animated_sprite = $AnimatedSprite2D
+
+# This checks if the enemy should be chasing the character
+var player_chase = false
+
+# Refers to the character when they enter the detection area.
+var player = null
+
+func _physics_process(delta):
+	# If player_chase is true, enemy should chase the character.
+	if player_chase:
+		position += (player.position - position)/SPEED
+		
+		animated_sprite.play("Walk")
+		
+		move_and_collide(Vector2(0,0))
+		
+		if(player.position.x - position.x) < 0:
+			animated_sprite.flip_h = true
+		else:
+			animated_sprite.flip_h = false
+	else:
+		animated_sprite.play("Idle")
+		
+		
+
+# Handles the character entering into the detection area.
+func _on_dectection_area_body_entered(body):
+	player = body
+	# Indicates to chase the player
+	player_chase = true
+	
+# Handles the character exiting the detection area.
+func _on_dectection_area_body_exited(body):
+	# Stops chasing the character.
+	player = null
+	player_chase = false
+
