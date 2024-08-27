4f5a486903a0e842f65be03d57e62efe2cb3c73d
bnuaWSCW <159732805+bnuaWSCW@users.noreply.github.com>
2024-08-27 15:09:35 +1200

    Brandon commit

--- Enemy.gd
+++ Enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 # The Speed of the Character
-@export var SPEED = 30
+@export var SPEED = 50
 @onready var animated_sprite = $AnimatedSprite2D
 
 # This checks if the enemy should be chasing the character
@@ -9,6 +9,9 @@ var player_chase = false
 
 # Refers to the character when they enter the detection area.
 var player = null
+
+var health = 100
+var player_in_zone = false
 
 func _physics_process(delta):
 	# If player_chase is true, enemy should chase the character.
@@ -40,3 +43,14 @@ func _on_dectection_area_body_exited(body):
 	player = null
 	player_chase = false
 
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_in_zone = true
+
+
+func _on_enemy_hitbox_body_exited(body):
+	pass # Replace with function body.
