7f047a569435b63b2a70c58f6f58dffb4de67a32
bnuaWSCW <159732805+bnuaWSCW@users.noreply.github.com>
2024-09-19 10:27:44 +1200

    fdsafdsdsffdsdsf

--- Menu.gd
+++ Menu.gd
@@ -5,7 +5,7 @@ func _on_play_pressed():
 	
 
 func _on_options_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 
 func _on_quit_pressed():
