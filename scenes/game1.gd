0ec57eec2afe5cabc6a5bfc37960c10c941c3073
bnuaWSCW <159732805+bnuaWSCW@users.noreply.github.com>
2024-09-26 10:28:11 +1200

    Brandon Commit 26/September/2024

--- scenes/game1.gd
+++ scenes/game1.gd
@@ -1,19 +1,28 @@
 extends Node2D
 
+# -- VARIABLES --#
 @onready var pause_menu = $"CharacterBody2D/Camera2D2/Pause Menu"
 var paused = false
+
+#-- FUNCTIONS --#
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
+		# if the player presses the escape button/pause button, do the pauseMenu function
 		pauseMenu()
 
 func pauseMenu():
+	# if paused is true
 	if paused:
+		#hide the pause menu and resume the game
 		pause_menu.hide()
 		get_tree().paused = false
+	# if paused is false
 	else:
+		# show the pause menu and pause the game
 		pause_menu.show()
 		get_tree().paused = true
 		
+	# whatever pause is right now, make it the opposite(if pause = true, !pause = false)
 	paused = !paused
